50577daaf98e9f65f12dee9a1524d5606eba2e1a
vvodianytskyi <vvodianytskyi@griddynamics.com>
2022-06-02 11:20:57 +0300

    Microservices learning shopping cart UI initial commit

---
+++ src/main/java/com/example/microserviceslearningshoppingcartui/MicroservicesLearningShoppingcartUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceslearningshoppingcartui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesLearningShoppingcartUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesLearningShoppingcartUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microserviceslearningshoppingcartui/MicroservicesLearningShoppingcartUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceslearningshoppingcartui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesLearningShoppingcartUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
